31bfe65d236c27054a0d1babe44dacc772504477
Iman Syaefulloh <imansyaefulloh@gmail.com>
2020-04-11 16:27:02 +0700

    passing todosList to todoItem

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 class TodosItem extends React.Component {
   render() {
     return (
       <div>
-        {this.props.todos.map((todo, i) => (
-          <li key={i}>{todo.title}</li>
+        {this.props.todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
         ))}
       </div>
     );
