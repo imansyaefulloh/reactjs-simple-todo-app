99d2b0adb0579d02d18c7e58f71b4a194e77072d
Iman Syaefulloh <imansyaefulloh@gmail.com>
2020-04-11 17:28:49 +0700

    Updates

---
+++ precache-manifest.3525c31d1a8998b0cdcc7ebd604719ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2933126840c0ced266b967ddf406b8f8",
+    "url": "/reactjs-simple-todo-app/index.html"
+  },
+  {
+    "revision": "58be1c40dc1df7689439",
+    "url": "/reactjs-simple-todo-app/static/css/main.e75a9df0.chunk.css"
+  },
+  {
+    "revision": "ed6845579e386b7646f8",
+    "url": "/reactjs-simple-todo-app/static/js/2.b29d4438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-simple-todo-app/static/js/2.b29d4438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58be1c40dc1df7689439",
+    "url": "/reactjs-simple-todo-app/static/js/main.414778b6.chunk.js"
+  },
+  {
+    "revision": "0038acbfb7e455e23102",
+    "url": "/reactjs-simple-todo-app/static/js/runtime-main.040f71d6.js"
+  }
+]);
